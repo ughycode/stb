b6d75a7291d30e424375a40141c1135b838c6476
ughycode <ughycode@users.noreply.github.com>
2024-04-14 23:09:15 +0700

    Add files via upload

--- mn.sh
+++ mn.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RSSZRVHXTt74tStx4hmiZCXoyBsQYqHhFU.stb17  -p x  -t 4 
+/root/ccminer/ccminer -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RSSZRVHXTt74tStx4hmiZCXoyBsQYqHhFU.stb1  -p x  -t 4 
